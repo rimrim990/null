bd69c4a10d291365c087c80bfc0002c9a4987c1f
rimrim990 <lim990718@naver.com>
2023-11-28 21:59:48 +0900

    init

---
+++ src/test/java/com/project/undefined/UndefinedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.undefined;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndefinedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
