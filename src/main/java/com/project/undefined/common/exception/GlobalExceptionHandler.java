69d43a78e6b35abf8f97a6f5d164d4ffc78e706a
rimrim990 <lim990718@naver.com>
2023-12-14 19:31:17 +0900

    feat: 유효하지 않은 enum 입력으로 인한 예외 핸들링

--- src/main/java/com/project/undefined/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/undefined/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.project.undefined.common.exception;
 import com.project.undefined.common.dto.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleCompanyException(final BaseException exception) {
         final ErrorResponse response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(response);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException() {
+        final ErrorResponse response = ErrorResponse.from("유효한 입력이 아닙니다.");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(response);
     }
--- src/test/java/com/project/undefined/acceptance/company/CompanyTest.java
+++ src/test/java/com/project/undefined/acceptance/company/CompanyTest.java
@@ -3,6 +3,8 @@ package com.project.undefined.acceptance.company;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.undefined.acceptance.AcceptanceTest;
 import com.project.undefined.acceptance.utils.RestAssuredUtils;
 import com.project.undefined.common.dto.response.ErrorResponse;
@@ -14,6 +16,7 @@ import com.project.undefined.company.repository.CompanyRepository;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.HashMap;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,6 +30,9 @@ public class CompanyTest extends AcceptanceTest {
 
     @Autowired
     private CompanyRepository companyRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     @DisplayName("등록된 모든 Company 목록을 조회한다")
@@ -83,6 +89,54 @@ public class CompanyTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         final Company company = RestAssuredUtils.extract(response, Company.class);
         assertThat(company.getId()).isEqualTo(companyId);
+    }
+
+    @Test
+    @DisplayName("Company 생성시 잘못된 Series 값을 넘기면 400 상태를 반환한다")
+    void create_invalidSeries_badRequest() throws JsonProcessingException {
+        // given
+        final HashMap<String, Object> request = new HashMap<>();
+        request.put("name", "test");
+        request.put("series", "invalid");
+        request.put("region", "SEOUL");
+
+        // when
+        final ExtractableResponse<Response> response = given().log().all()
+            .when()
+            .body(objectMapper.writeValueAsString(request))
+            .contentType(ContentType.JSON)
+            .post("/companies/" )
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        final ErrorResponse error = RestAssuredUtils.extract(response, ErrorResponse.class);
+        assertThat(error.getMessage()).isEqualTo("유효한 입력이 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("Company 생성시 잘못된 Region 값을 넘기면 400 상태를 반환한다")
+    void create_invalidRegion_badRequest() throws JsonProcessingException {
+        // given
+        final HashMap<String, Object> request = new HashMap<>();
+        request.put("name", "test");
+        request.put("series", "A");
+        request.put("region", "invalid");
+
+        // when
+        final ExtractableResponse<Response> response = given().log().all()
+            .when()
+            .body(objectMapper.writeValueAsString(request))
+            .contentType(ContentType.JSON)
+            .post("/companies/" )
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        final ErrorResponse error = RestAssuredUtils.extract(response, ErrorResponse.class);
+        assertThat(error.getMessage()).isEqualTo("유효한 입력이 아닙니다.");
     }
 
     @Test
