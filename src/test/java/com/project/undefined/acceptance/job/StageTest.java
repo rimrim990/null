69a501c8afe7ae91b8103bda19c97c3de209ba42
rimrim990 <lim990718@naver.com>
2023-12-15 20:15:37 +0900

    test: 목 데이터를 사용하도록 수정

--- src/test/java/com/project/undefined/acceptance/job/StageTest.java
+++ src/test/java/com/project/undefined/acceptance/job/StageTest.java
@@ -6,13 +6,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.project.undefined.acceptance.AcceptanceTest;
 import com.project.undefined.acceptance.utils.RestAssuredUtils;
 import com.project.undefined.job.dto.request.CreateStageRequest;
+import com.project.undefined.job.entity.Job;
+import com.project.undefined.job.repository.JobRepository;
 import com.project.undefined.job.repository.StageRepository;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 
 @DisplayName("Stage API 인수 테스트")
@@ -21,11 +26,15 @@ public class StageTest extends AcceptanceTest {
     @Autowired
     private StageRepository stageRepository;
 
+    @Autowired
+    private JobRepository jobRepository;
+
     @Test
     @DisplayName("Stage를 생성한다.")
     void create_created() {
         // given
-        final CreateStageRequest request = new CreateStageRequest(1L, "test");
+        final Long jobId = getJobIds(Pageable.ofSize(1)).get(0);
+        final CreateStageRequest request = new CreateStageRequest(jobId, "test");
 
         // when
         final ExtractableResponse<Response> response = given().log().all()
@@ -40,5 +49,13 @@ public class StageTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         final Long stageId = RestAssuredUtils.parseLocationId(response, "/stages/");
         assertThat(stageRepository.existsById(stageId)).isTrue();
+    }
+
+    private List<Long> getJobIds(final Pageable pageable) {
+        final Page<Job> jobs = jobRepository.findAll(pageable);
+        return jobs.getContent()
+            .stream()
+            .map(Job::getId)
+            .toList();
     }
 }
