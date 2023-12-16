616b5c8291a3882c6699d3f99ca39dafe55051bd
rimrim990 <lim990718@naver.com>
2023-12-17 00:06:56 +0900

    test: Stage 생성시 파라미터를 검증히는 인수 테스트

--- src/test/java/com/project/undefined/acceptance/retrospect/RetrospectTest.java
+++ src/test/java/com/project/undefined/acceptance/retrospect/RetrospectTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.project.undefined.acceptance.AcceptanceTest;
 import com.project.undefined.acceptance.utils.RestAssuredUtils;
+import com.project.undefined.common.dto.response.ErrorResponse;
 import com.project.undefined.job.entity.Stage;
 import com.project.undefined.job.repository.StageRepository;
 import com.project.undefined.retrospect.dto.request.CreateRetrospectRequest;
@@ -16,6 +17,8 @@ import io.restassured.response.Response;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -51,6 +54,98 @@ public class RetrospectTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         final Long retrospectId = RestAssuredUtils.parseLocationId(response, "/retrospects/");
         assertThat(retrospectRepository.existsById(retrospectId)).isTrue();
+    }
+
+    @Test
+    @DisplayName("stageId가 null이면 400 상태를 반환한다.")
+    void create_nullStageId_badRequest() {
+        // given
+        final CreateRetrospectRequest request = new CreateRetrospectRequest(null, "test", "good",
+            "bad", (short) 3, "not bad");
+
+        // when
+        final ExtractableResponse<Response> response = given().log().all()
+            .when()
+            .body(request)
+            .contentType(ContentType.JSON)
+            .post("/retrospects/")
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        final ErrorResponse error = RestAssuredUtils.extract(response, ErrorResponse.class);
+        assertThat(error.getMessage()).isEqualTo("stageId 은(는) 필수 입력 값입니다.");
+    }
+
+    @Test
+    @DisplayName("stageId가 유효하지 않으면 400 상태를 반환한다.")
+    void create_invalidStageId_badRequest() {
+        // given
+        final long invalidStageId = 1_000_000;
+        final CreateRetrospectRequest request = new CreateRetrospectRequest(invalidStageId, "test", "good",
+            "bad", (short) 3, "not bad");
+
+        // when
+        final ExtractableResponse<Response> response = given().log().all()
+            .when()
+            .body(request)
+            .contentType(ContentType.JSON)
+            .post("/retrospects/")
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        final ErrorResponse error = RestAssuredUtils.extract(response, ErrorResponse.class);
+        assertThat(error.getMessage()).isEqualTo("일치하는 Stage가 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("content가 공백이면 400 상태를 반환한다.")
+    void create_blankContent_badRequest() {
+        // given
+        final Long stageId = getStageIds(Pageable.ofSize(1)).get(0);
+        final CreateRetrospectRequest request = new CreateRetrospectRequest(stageId, "", "good",
+            "bad", (short) 3, "not bad");
+
+        // when
+        final ExtractableResponse<Response> response = given().log().all()
+            .when()
+            .body(request)
+            .contentType(ContentType.JSON)
+            .post("/retrospects/")
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        final ErrorResponse error = RestAssuredUtils.extract(response, ErrorResponse.class);
+        assertThat(error.getMessage()).isEqualTo("content 은(는) 필수 입력 값이며 공백을 제외한 문자를 하나 이상 포함해야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(shorts = {0, 6})
+    @DisplayName("score가 1이상 5이하의 정수가 아니면 400 상태를 반환한다.")
+    void create_invalidScore_badRequest(final short score) {
+        // given
+        final Long stageId = getStageIds(Pageable.ofSize(1)).get(0);
+        final CreateRetrospectRequest request = new CreateRetrospectRequest(stageId, "test", "good",
+            "bad", score, "not bad");
+
+        // when
+        final ExtractableResponse<Response> response = given().log().all()
+            .when()
+            .body(request)
+            .contentType(ContentType.JSON)
+            .post("/retrospects/")
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        final ErrorResponse error = RestAssuredUtils.extract(response, ErrorResponse.class);
+        assertThat(error.getMessage()).isEqualTo("score 은(는) 5이하 1이상의 값이어야 합니다.");
     }
 
     private List<Long> getStageIds(final Pageable pageable) {
