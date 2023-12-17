7c87b8bac5246a6070d0585b3c690f4ee9b2df1e
rimrim990 <lim990718@naver.com>
2023-12-17 23:00:27 +0900

    refactor: 미사용 API 제거

--- src/main/java/com/project/undefined/retrospect/controller/RetrospectController.java
+++ src/main/java/com/project/undefined/retrospect/controller/RetrospectController.java
@@ -1,17 +1,10 @@
 package com.project.undefined.retrospect.controller;
 
-import com.project.undefined.retrospect.dto.request.CreateRetrospectRequest;
 import com.project.undefined.retrospect.dto.response.RetrospectResponse;
 import com.project.undefined.retrospect.service.RetrospectService;
-import jakarta.validation.Valid;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,14 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RetrospectController {
 
     private final RetrospectService retrospectService;
-
-    @PostMapping("/")
-    public ResponseEntity<Void> create(@Valid @RequestBody final CreateRetrospectRequest request) {
-        final RetrospectResponse retrospectResponse = retrospectService.create(request);
-        return ResponseEntity.status(HttpStatus.CREATED.value())
-            .location(URI.create("/retrospects/" + retrospectResponse.getId()))
-            .build();
-    }
 
     @GetMapping("/{id}")
     public RetrospectResponse get(@PathVariable final Long id) {
