1982e69a3abfd12fd3f929f69996c96aa95df8b2
rimrim990 <lim990718@naver.com>
2023-12-17 14:21:52 +0900

    test: Retrospect 목 데이터 생성

--- src/main/resources/testdb/data-h2.sql
+++ src/main/resources/testdb/data-h2.sql
@@ -14,3 +14,6 @@ insert into stage(job_id, name, state, created_at, last_modified_at)
     select id, '코딩 테스트', 'PASS', now(), now() from job;
 insert into stage(job_id, name, state, created_at, last_modified_at)
     select id, '1차 면접', 'WAIT', now(), now() from job;
+
+insert into retrospect(stage_id, content, good_point, bad_point, summary, score, created_at, last_modified_at)
+    select id, 'test', 'good', 'bad', 'summary', 3, now(), now() from stage;
